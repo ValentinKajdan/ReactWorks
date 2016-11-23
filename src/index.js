ef630aaed5e5981a2856e405c5103c475b79997f
valentinkajdan <valentinkajdan@MC-G-LP-C120-10.local>
2016-11-23 14:37:48 +0100

    react

--- src/index.js
+++ src/index.js
@@ -1,41 +1,8 @@
 import ReactDOM from 'react-dom';
 import React    from 'react';
-
-function Comment(props) {
-  return (
-    <div className="Comment">
-      <div className="UserInfo">
-        <img
-          className="Avatar"
-          src={props.author.avatarUrl}
-          alt={props.author.name}
-        />
-        <div className="UserInfo-name">
-          {props.author.name}
-        </div>
-      </div>
-      <div className="Comment-text">
-        {props.text}
-      </div>
-      <div className="Comment-date">
-        {props.date}
-      </div>
-    </div>
-  );
-}
-Comment.propTypes = {
-  author: React.PropTypes.shape({
-    name: React.PropTypes.string,
-    avatarUrl: React.PropTypes.string,
-  }),
-  text: React.PropTypes.string,
-  date: React.PropTypes.string,
-};
+import Comment from './components/comment.js'
 
 const element = (<Comment
-  author={{
-    name: 'Mikaël',
-    avatarUrl: 'https://fr.gravatar.com/userimage/31654435/861121bab44174b542f625e11d796268.png?size=200' }}
   text={'I wrote a super comment on my blog'}
   date={new Date().toLocaleString()}
 />);
